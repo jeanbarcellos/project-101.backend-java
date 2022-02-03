675672d5bf4b0a4a1412d29ac77ed3d070e93f5b
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-03 10:24:59 -0300

    domain - Ajustes em category

--- src/main/java/com/jeanbarcellos/demo/domain/entities/Category.java
+++ src/main/java/com/jeanbarcellos/demo/domain/entities/Category.java
@@ -3,19 +3,28 @@ package com.jeanbarcellos.demo.domain.entities;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.jeanbarcellos.demo.core.domain.AggregateRoot;
-import com.jeanbarcellos.demo.core.domain.Entity;
 
 import lombok.Getter;
 
 @javax.persistence.Entity
 @Getter
 @Table(name = "category")
-public class Category extends Entity implements AggregateRoot {
+public class Category implements AggregateRoot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_seq")
+    @SequenceGenerator(name = "category_id_seq", sequenceName = "category_id_seq", allocationSize = 1)
+    @Column(name = "id", updatable = false)
+    protected Integer id;
 
     @Column(name = "name", nullable = false)
     public String name;
@@ -26,7 +35,15 @@ public class Category extends Entity implements AggregateRoot {
     @Column(name = "updated_at", nullable = false)
     public LocalDateTime updatedAt;
 
+    protected Category() {
+    }
+
     public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(Integer id, String name) {
+        this.id = id;
         this.name = name;
     }
 
--- src/test/java/com/jeanbarcellos/demo/domain/entities/CategoryTest.java
+++ src/test/java/com/jeanbarcellos/demo/domain/entities/CategoryTest.java
@@ -19,4 +19,18 @@ public class CategoryTest {
         // Assert
         assertEquals(name, category.getName());
     }
+
+    @Test
+    public void construct_entryIdAndName_shouldReturnNewCategory() {
+        // Arrange
+        Integer id = 999;
+        String name = "Futebol";
+
+        // Act
+        Category category = new Category(id, name);
+
+        // Assert
+        assertEquals(id, category.getId());
+        assertEquals(name, category.getName());
+    }
 }
