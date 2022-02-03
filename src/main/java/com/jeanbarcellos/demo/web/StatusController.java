4f12eb5afcde04eef673d07e6591751bb8711e2a
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-03 08:27:01 -0300

    Implementação inicial.

---
+++ src/main/java/com/jeanbarcellos/demo/web/StatusController.java
@@ -0,0 +1,24 @@
+package com.jeanbarcellos.demo.web;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/status")
+public class StatusController {
+
+    public ResponseEntity<HashMap<String, Object>> showStatus() {
+        var response = new HashMap<String, Object>();
+
+        response.put("service", "Proccess Manager API");
+        response.put("status", "up");
+        response.put("httpStatus", HttpStatus.OK.value());
+
+        return ResponseEntity.ok(response);
+    }
+
+}
