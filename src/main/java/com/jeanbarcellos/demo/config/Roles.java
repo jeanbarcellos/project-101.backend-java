730e08aaa481afa33f1b49150109d4408f6b1ed1
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-20 16:15:34 -0300

    config - Criação de Roles

---
+++ src/main/java/com/jeanbarcellos/demo/config/Roles.java
@@ -0,0 +1,13 @@
+package com.jeanbarcellos.demo.config;
+
+public class Roles {
+
+    // Somente desenvolvedor ou dono do software
+    public static final String ROOT = "root";
+
+    // Geral
+    public static final String ADMIN = "admin";
+    public static final String TRIADOR = "triador";
+    public static final String FINALIZADOR = "finalizador";
+
+}
