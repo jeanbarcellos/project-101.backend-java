a68e4d8a6a533bd52f63c1641012fc8e5e67be75
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-20 16:14:18 -0300

    Application - Criação do serviço para manipulação de User

---
+++ src/main/java/com/jeanbarcellos/demo/application/mappers/UserMapper.java
@@ -0,0 +1,56 @@
+package com.jeanbarcellos.demo.application.mappers;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.jeanbarcellos.demo.application.dtos.UserRequest;
+import com.jeanbarcellos.demo.application.dtos.UserResponse;
+import com.jeanbarcellos.demo.domain.entities.Role;
+import com.jeanbarcellos.demo.domain.entities.User;
+
+public class UserMapper {
+
+    public static User toUser(UserRequest request) {
+        var user = new User(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getStatus());
+
+        return user;
+    }
+
+    public static User toUser(UserRequest request, List<Role> roles) {
+        var user = new User(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getStatus());
+
+        for (Role role : roles) {
+            user.addRole(role);
+        }
+
+        return user;
+    }
+
+    public static User toUser(UUID id, UserRequest request, List<Role> roles) {
+        var user = new User(
+                id,
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getStatus());
+
+        for (Role role : roles) {
+            user.addRole(role);
+        }
+
+        return user;
+    }
+
+    public static UserResponse toResponse(User user) {
+        return UserResponse.from(user);
+    }
+
+}
