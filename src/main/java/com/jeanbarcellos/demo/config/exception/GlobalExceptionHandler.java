d53c18c7edd2b569657bf6293052b3e268903aea
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-20 15:23:34 -0300

    Config - Criação do manipulador global de Exceptions

---
+++ src/main/java/com/jeanbarcellos/demo/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.jeanbarcellos.demo.config.exception;
+
+import com.jeanbarcellos.demo.core.dtos.ErrorResponse;
+import com.jeanbarcellos.demo.core.exceptions.NotFoundException;
+import com.jeanbarcellos.demo.core.exceptions.ValidationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class GlobalExceptionHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException exception) {
+        logger.error(exception.getMessage());
+
+        var response = new ErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException exception) {
+        var response = new ErrorResponse();
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // Todas as demais exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception exception) {
+
+        logger.error(exception.getMessage());
+
+        var response = new ErrorResponse();
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage("Erro Interno do Servidor. Tente novamente mais tarde.");
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
