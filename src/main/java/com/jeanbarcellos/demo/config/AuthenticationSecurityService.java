2f035e8c3f676e6ea0434e057117f138796b4caf
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-02-22 22:17:31 -0300

    config - Configurando a autenticação usando o security

---
+++ src/main/java/com/jeanbarcellos/demo/config/AuthenticationSecurityService.java
@@ -0,0 +1,39 @@
+package com.jeanbarcellos.demo.config;
+
+import com.jeanbarcellos.demo.domain.entities.User;
+import com.jeanbarcellos.demo.domain.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Serviço para obtenção do usuário pelo username (email)
+ * Utiliada somente pelo Security
+ */
+@Service
+@Slf4j
+public class AuthenticationSecurityService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if (user == null) {
+            log.error("Usuário não encontrado: {}", username);
+            throw new UsernameNotFoundException("Usuário não enconrado");
+        }
+
+        log.info("Usuário encontrado: {}", username);
+
+        return user;
+    }
+
+}
--- src/main/java/com/jeanbarcellos/demo/config/WebSecurityConfig.java
+++ src/main/java/com/jeanbarcellos/demo/config/WebSecurityConfig.java
@@ -1,22 +1,48 @@
 package com.jeanbarcellos.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] ENDPOINTS_PUBLIC = {
+            "/auth/**",
+            "/utils/**"
+    };
+
+    @Autowired
+    private AuthenticationSecurityService authenticationService;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    // Configurations for authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     // Configuration for authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll().and() // Permitir qualquer request
+                .antMatchers(ENDPOINTS_PUBLIC).permitAll() // Permitir acesso apenas de /auth
+                .anyRequest().authenticated().and() // Demais requests devem estar autenticados
                 .csrf().disable() // Desabilita a política CSRF
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // gerenciamento de sessão;
         ;
