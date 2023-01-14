9b50a9982d1e1317a188e8635233ae372250c005
Jean Barcellos <jeanbarcellos@hotmail.com>
2023-01-14 16:41:23 -0300

    Ajuste nas configurações

--- src/main/java/com/jeanbarcellos/demo/config/OpenAPIConfig.java
+++ src/main/java/com/jeanbarcellos/demo/config/OpenAPIConfig.java
@@ -1,5 +1,6 @@
 package com.jeanbarcellos.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,25 +9,32 @@ import com.jeanbarcellos.demo.config.constants.APIConstants;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenAPIConfig {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spring.application.description}")
+    private String appDescription;
 
     @Bean
     public OpenAPI springShopOpenAPI() {
 
         var config = new OpenAPI();
 
-        config.info(new Info()
-                .title("Demo API")
-                .description("App demo para explorar recursos do Spring")
-                .version("v0.0.1")
-                .license(new License()
-                        .name("Apache 2.0")
-                        .url("http://springdoc.org")));
+        var info = new Info()
+                .title(appName)
+                .description(appDescription)
+                .version("v0.0.1");
+        info.contact(new Contact()
+                .name("Jean Barcellos")
+                .url("www.jeanbarcellos.com.br"));
+        config.info(info);
 
         config.externalDocs(new ExternalDocumentation()
                 .description("Project 101 Documentation")
