82c30bd5240c38df8807ecb84ee7e9e6a466b89b
Jean Barcellos <jeanbarcellos@hotmail.com>
2023-03-27 22:04:45 -0300

    Ajuste nos DTOs

--- src/main/java/com/jeanbarcellos/project101/application/dtos/AuthenticationLoginRequest.java
+++ src/main/java/com/jeanbarcellos/project101/application/dtos/AuthenticationLoginRequest.java
@@ -3,9 +3,15 @@ package com.jeanbarcellos.project101.application.dtos;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Builder
+@AllArgsConstructor
+@Accessors(chain = true)
 public class AuthenticationLoginRequest {
 
     @NotBlank
--- src/main/java/com/jeanbarcellos/project101/application/dtos/AuthenticationLoginWithTokenRequest.java
+++ src/main/java/com/jeanbarcellos/project101/application/dtos/AuthenticationLoginWithTokenRequest.java
@@ -2,9 +2,15 @@ package com.jeanbarcellos.project101.application.dtos;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Builder
+@AllArgsConstructor
+@Accessors(chain = true)
 public class AuthenticationLoginWithTokenRequest {
 
     @NotBlank
