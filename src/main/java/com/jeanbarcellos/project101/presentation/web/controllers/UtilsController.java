ae2ced107bffe71b56495e8d8541b02497898924
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-11-20 18:45:40 -0300

    Ajustes

--- src/main/java/com/jeanbarcellos/project101/presentation/web/controllers/UtilsController.java
+++ src/main/java/com/jeanbarcellos/project101/presentation/web/controllers/UtilsController.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +30,7 @@ import lombok.Data;
 public class UtilsController extends ControllerBase {
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/guid-generate")
     @Operation(summary = "Gerar GUID/UUID", description = "Gera um token GUID", security = {
