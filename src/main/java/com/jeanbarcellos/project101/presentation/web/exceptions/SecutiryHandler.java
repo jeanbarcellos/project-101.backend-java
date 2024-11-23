06f5aa2b8f73fb1ede2182e54e9f531ace253a43
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-11-23 18:32:27 -0300

    Ajustes no manipulador de exceções do Security

--- src/main/java/com/jeanbarcellos/project101/presentation/web/exceptions/SecutiryHandler.java
+++ src/main/java/com/jeanbarcellos/project101/presentation/web/exceptions/SecutiryHandler.java
@@ -9,14 +9,15 @@ import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.jeanbarcellos.core.dto.ErrorResponse;
-import com.jeanbarcellos.core.exception.AuthenticationException;
-import com.jeanbarcellos.core.exception.AuthorizationException;
+import com.jeanbarcellos.core.exception.JWTAuthenticationException;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -28,73 +29,55 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class SecutiryHandler {
 
-    // #region Autenticação
-
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handle(AuthenticationException exception) {
+    // Autentication JEAN
+    @ExceptionHandler(JWTAuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handle(JWTAuthenticationException exception) {
         return createResponseUnauthorized(exception.getMessage());
     }
 
-    @ExceptionHandler(LockedException.class)
-    public ResponseEntity<ErrorResponse> handle(LockedException exception) {
-        return createResponseUnauthorized("A conta do usuário está bloqueada.");
-    }
-
-    @ExceptionHandler(DisabledException.class)
-    public ResponseEntity<ErrorResponse> handle(DisabledException exception) {
-        return createResponseUnauthorized("O usuário está desabilitado.");
-    }
-
-    @ExceptionHandler(AccountStatusException.class)
-    public ResponseEntity<ErrorResponse> handle(AccountStatusException exception) {
-        return createResponseUnauthorized(
-                "Erro ao tentar autenticar com o usuário. Entre em contato com o administrador do sistema");
-    }
-
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handle(UsernameNotFoundException exception) {
-        return createResponseUnauthorized("Usuário não encontrado.");
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handle(BadCredentialsException exception) {
-        return createResponseUnauthorized("Credenciais iváliadas.");
-    }
-
-    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handle(AuthenticationCredentialsNotFoundException exception) {
-        return createResponseUnauthorized("Credenciais não encontradas.");
-    }
-
-    @ExceptionHandler(org.springframework.security.core.AuthenticationException.class)
-    public ResponseEntity<ErrorResponse> handle(
-            org.springframework.security.core.AuthenticationException exception) {
-        log.error(exception.getMessage());
-        return createResponseUnauthorized("Erro de autenticação.");
-    }
-
-    // #endregion
-
-    // #region Autorização
-
-    @ExceptionHandler(AuthorizationException.class)
-    public ResponseEntity<ErrorResponse> handle(AuthorizationException exception) {
-        return createResponseForbidden(exception.getMessage());
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handle(AuthenticationException exception) {
+        return createResponseUnauthorized(String.format("Erro de autenticação: %s", getMessage(exception)));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handle(AccessDeniedException exception) {
-        return createResponseUnauthorized("Acesso não autorizado.");
+        return createResponseForbidden("Acesso não autorizado.");
     }
 
-    // #endregion
-
+    // 401
     private static ResponseEntity<ErrorResponse> createResponseUnauthorized(String message) {
         return new ResponseEntity<>(ErrorResponse.unauthorized(message), HttpStatus.UNAUTHORIZED);
     }
 
+    // 403
     private static ResponseEntity<ErrorResponse> createResponseForbidden(String message) {
         return new ResponseEntity<>(ErrorResponse.forbidden(message), HttpStatus.FORBIDDEN);
+    }
+
+    private static String getMessage(AuthenticationException exception) {
+        var msg = "";
+        var clazz = exception.getClass();
+
+        if (clazz == LockedException.class) {
+            msg = "A conta do usuário está bloqueada.";
+        } else if (clazz == DisabledException.class) {
+            msg = "O usuário está desabilitado.";
+        } else if (clazz == AccountStatusException.class) {
+            msg = "Problemas no status da conta";
+        } else if (clazz == UsernameNotFoundException.class) {
+            msg = "Usuário não encontrado.";
+        } else if (clazz == BadCredentialsException.class) {
+            msg = "Credenciais iváliadas.";
+        } else if (clazz == AuthenticationCredentialsNotFoundException.class) {
+            msg = "Credenciais não encontradas.";
+        } else if (clazz == InsufficientAuthenticationException.class) {
+            msg = "Credenciais insuficientes";
+        } else {
+            msg = "";
+        }
+
+        return msg;
     }
 
 }
